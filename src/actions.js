ef10b15624a10f02b2bf429468969de2b7f7a736
Andy Gaines <andyagaines@gmail.com>
2017-11-28 15:13:46 -0600

    first async fully functional

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,29 @@
-export const fetchSubscription = () => dispatch => {};
+// sync
+export const FETCH_REQUEST = 'FETCH_REQUEST';
+export const fetchRequest = () => ({
+	type: FETCH_REQUEST
+});
+
+export const FETCH_ERROR = 'FETCH_ERROR';
+export const fetchError = error => ({
+	type: FETCH_ERROR,
+	error
+});
+
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const fetchSuccess = subscriptions => ({
+	type: FETCH_SUCCESS,
+	subscriptions
+});
+
+
+//async
+// export const fetchSubscription = () => dispatch => {};
 
 export const FETCH_ALL_SUBSCRIPTIONS = 'FETCH_ALL_SUBSCRIPTIONS';
 export const fetchAllSubscriptions = () => dispatch => {
-	fetch('http://localhost:8080/api/subscriptions', {
+	dispatch(fetchRequest());
+	return fetch('http://localhost:8080/api/subscriptions', {
 		method: 'GET',
 		headers: {'Content-Type': 'application/json', 'Accept': 'application/json' , 'user_id': '1'}
 	}).then(res => {
@@ -10,5 +31,6 @@ export const fetchAllSubscriptions = () => dispatch => {
 			return Promise.reject(res.statusText);
 		}
 		return res.json();
-	}).then(subs => console.log(subs));
+	}).then(subs => dispatch(fetchSuccess(subs)))
+		.catch(err => {dispatch(fetchError(err));});
 };
